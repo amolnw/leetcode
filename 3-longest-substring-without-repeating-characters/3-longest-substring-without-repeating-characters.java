7832f9840faaa0af32a0a6694b7b2c1503daf792
Amol Nandkishor Wadwalkar <amolnw@hotmail.com>
2022-05-07 16:04:08 -0700

    Update 3-longest-substring-without-repeating-characters.java

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -4,6 +4,9 @@ class Solution {
         Map<Character, Integer> charMap = new HashMap<>();
         
         for(int i = 0; i < s.length(); i++) {
+            /*
+            charMap.get(s.charAt(i)) >= start) is important for cases like abba
+            */
             if ( charMap.containsKey(s.charAt(i) ) && charMap.get(s.charAt(i)) >= start) {
                 result = Math.max(i - start, result);
                 start = charMap.get(s.charAt(i)) + 1;
@@ -11,6 +14,9 @@ class Solution {
             charMap.put(s.charAt(i), i);    
         }
         
+        /*
+        This is important for handling last case
+        */
         return Math.max(s.length() - start, result);
     }
 }
