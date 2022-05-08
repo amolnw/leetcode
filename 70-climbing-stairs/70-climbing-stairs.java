039dd39dbfcac61ae929430feb99a3a1f0fd2d75
Amol Nandkishor Wadwalkar <amolnw@hotmail.com>
2022-05-08 02:39:26 -0700

    Time: 0 ms (100.00%), Space: 40.7 MB (59.82%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,24 +1,18 @@
-/*
-1 - 1
-2 - 2
-3 - 1,1,1 1,2 2,1 - 3
-4 - 1,1,1,1 1,2,1 2,1,1 1,2,2 2,2    
-*/
 class Solution {
     public int climbStairs(int n) {
-        int first = 1, second = 2, result = 0;
-        if ( n == 1 ) 
-            return first;
+        int f = 1, s = 2;
+        if ( n == 1 )
+            return f;
         else if ( n == 2 )
-            return second;
+            return s;
         else {
             for(int i = 3; i <= n; i++) {
-                result = first + second;
-                first = second;
-                second = result;
+                int temp = s;
+                s = temp + f;
+                f = temp;
             }
         }
         
-        return result;
+        return s;
     }
 }
