9a96cb270b0f567c1366e04e3aca3c412510fe63
Amol Nandkishor Wadwalkar <amolnw@hotmail.com>
2022-05-08 14:15:48 -0700

    Update 55-jump-game.java

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -3,6 +3,7 @@ class Solution {
         int lastReachablePosition = nums.length - 1;
         
         for(int i = nums.length - 1; i >= 0; i--) {
+            //Check if you can reach the last reachable position 
             if ( i + nums[i] >= lastReachablePosition ) 
                 lastReachablePosition = i;
         }
