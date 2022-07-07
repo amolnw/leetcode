c68b1a1cecdc560a48fe4bded3984516052d0b37
Amol Nandkishor Wadwalkar <amolnw@hotmail.com>
2022-07-07 01:41:16 -0700

    Time: 22 ms (16.94%), Space: 50.9 MB (20.09%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<Integer> sortedOrder = new ArrayList<>();
+        Map<Integer, Integer> inDegree = new HashMap<>();
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        
+        for(int i=0; i<numCourses; i++) {
+            inDegree.put(i, 0);
+            graph.put(i, new ArrayList<>());
+        }
+        
+        for(int[] prerequisite: prerequisites) {
+            int parent = prerequisite[1];
+            int child = prerequisite[0];
+            
+            inDegree.put(child, inDegree.get(child)+1);
+            graph.get(parent).add(child);
+        }
+        
+        Queue<Integer> sources = new LinkedList<>();
+        for(Map.Entry<Integer, Integer> entry: inDegree.entrySet()) {
+            if ( entry.getValue() == 0 ) {
+                sources.add(entry.getKey());
+            }
+        }
+        
+        while(!sources.isEmpty()) {
+            int source = sources.poll();
+            sortedOrder.add(source);
+            List<Integer> children = graph.get(source);
+            
+            for(int child: children) {
+                inDegree.put(child, inDegree.get(child)-1);
+                if(inDegree.get(child) == 0) {
+                    sources.add(child);
+                }
+            }
+        }
+        
+        if ( sortedOrder.size() == numCourses ) {
+            return sortedOrder.stream().mapToInt(Integer::intValue).toArray();
+        }
+            
+        
+        return new int[0];
+    }
+}
